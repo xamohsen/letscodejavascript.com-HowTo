370e82a59b61a95332d37197a825cdaab808888d
Ahmed Mohsen <xamohsen@apples-mbp.lan>
2018-08-30 14:50:26 +0200

    add linting to jake.sh

--- Jakefile.js
+++ Jakefile.js
@@ -1,10 +1,29 @@
 (function(){
     "use static";
-    console.log(" \n\n Build oki");
+
 
     desc("default build");
-    task("default", function () {
-        console.log('hello, im the defaut task');
+    task("default",["version", "lint"], function () {
+        console.log('hello, im the default task');
     });
+
+    desc("check node version");
+    task("version", function () {
+        console.log("checking node version: .");
+
+        var packageJson = require("./package.json");
+        var expectedVersion = "v" + packageJson.engines.node;
+        var actualVersion = process.version;
+        if(actualVersion !== expectedVersion) {
+            fail("Incorrect Node version: expected " + expectedVersion + " but was " + actualVersion);
+        }
+        console.log(process.version);
+    });
+
+    desc("lint the code");
+    task("lint", function () {
+        console.log("linting js ..");
+        jake.exec("node node_modules/jshint/bin/jshint Jakefile.js", {interactive: true}, complete);
+    }, {async: true});
 
 }());
